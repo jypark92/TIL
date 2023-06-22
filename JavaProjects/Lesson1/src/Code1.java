750208166adc099220020379c265aa781dea6ce0
jypark92 <jyp921111@gmail.com>
2023-06-23 01:52:15 +0900

    Add: 파일 추가

---
+++ JavaProjects/Lesson1/src/Code1.java
@@ -0,0 +1,19 @@
+public class Code1 {
+
+    static int num;
+
+    public static void main(String[] args) {
+        int anotherNum = 5;
+        num = 2;
+
+        System.out.println(num + anotherNum);
+
+        System.out.println("Num: " + num);
+
+        System.out.println("AnotherNum: " + anotherNum);
+
+        System.out.println("Sum: " + num + anotherNum); // left associativity
+
+        System.out.println("Sum: " + (num + anotherNum));
+    }
+}
---
+++ JavaProjects/Lesson1/src/Hello.java
@@ -0,0 +1,5 @@
+public class Hello {
+    public static void main(String[] args) {    // main method
+        System.out.println("Hello Java !!!");
+    }
+}
