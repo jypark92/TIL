edecbf6500d48931417ff815401255992dec099d
jypark92 <jyp921111@gmail.com>
2023-06-24 12:39:15 +0900

    Add: 파일 추가

--- JavaProjects/Lesson1/src/Code2.java
+++ JavaProjects/Lesson1/src/Code2.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Code2 {
+import java.util.Scanner;
+public class Code2 {
+    public static void main(String[] args) {
+        
+        int number = 123;
+        
+        Scanner kb = new Scanner(System.in);
+        
+        System.out.print("Please enter an integer: ");
+        
+        int input = kb.nextInt();
+        
+        if(input == number) {
+            System.out.println("Number match! :-)");
+        }
+        else {
+            System.out.println("Numbers do not match! :-(");
+        }
+
+        kb.close();
+    }
 }
