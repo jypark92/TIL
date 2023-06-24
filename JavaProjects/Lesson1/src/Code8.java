35b0ecac579be1b90570dc8c2814051c962053eb
jypark92 <jyp921111@gmail.com>
2023-06-24 22:32:42 +0900

    Add: 파일추가

---
+++ JavaProjects/Lesson1/src/Code8.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Code8 {
+    public static void main(String[] args) {
+
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int [] data = new int [n];
+        for (int i = 0; i < n; i++)
+            data[i] = kb.nextInt();
+        kb.close();
+
+        int sum = 0;
+        int max = data[0];
+        for (int i = 0; i < n; i++){
+            sum += data[i];
+            if (data[i] > max)
+                max = data[i];
+        }
+
+        System.out.println("The sum is " + sum);
+        System.out.println("The max is " + max);
+    }
+}
